97f0a95bde5aca800210f4036bcbb14d63ff4c7e
awaidea <awa@ghriver.top>
2024-09-06 10:23:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
    const express = require('express');
    const app = express();
+   app.use(express.static('public'));
+   app.get('/', (req, res) => {
+     res.send(`
+       <html>
+         <head>
+           <title>CETA 主页</title>
+           <link rel="stylesheet" href="/styles.css">
+         </head>
+         <body>
+           <h1>欢迎来到 CETA 组织的网站</h1>
+           <p>qwq</p>
+         </body>
+       </html>
+     `);
+   });
